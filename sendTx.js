9b6c6e10104b73591eeb8107321e6098348f99d1
ursister <106648827+ursisterbtw@users.noreply.github.com>
2023-10-25 19:28:38 -0400

    Add files via upload

---
+++ sendTx.js
@@ -0,0 +1,37 @@
+const { Alchemy, Network, Wallet, Utils } = require("alchemy-sdk");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const { API_KEY, PRIVATE_KEY1 } = process.env;
+
+const settings = {
+  apiKey: API_KEY,
+  network: Network.ETH_SEPOLIA,
+};
+const alchemy = new Alchemy(settings);
+
+let wallet = new Wallet(PRIVATE_KEY1);
+
+async function main() {
+  const nonce = await alchemy.core.getTransactionCount(
+    wallet.address,
+    "latest"
+  );
+
+  let transaction = {
+    to: "0x079A733703745fb6B3499677e054eb96a1A697A9",
+    value: Utils.parseEther("0.0"),
+    gasLimit: "21000",
+    maxPriorityFeePerGas: Utils.parseUnits("5", "gwei"),
+    maxFeePerGas: Utils.parseUnits("20", "gwei"),
+    nonce: nonce,
+    type: 2,
+    chainId: 11155111,
+  };
+
+  let rawTransaction = await wallet.signTransaction(transaction);
+  let tx = await alchemy.core.sendTransaction(rawTransaction);
+  console.log("Sent transaction", tx);
+}
+
+main();
